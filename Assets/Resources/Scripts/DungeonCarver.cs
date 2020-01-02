f2c6640f55e85b0f6dd0bf3adcbcecd3df0bf9e7
Fixtone <tonet.capdevila@gmail.com>
2020-01-02 16:05:05 +0100

    Format Files

--- Assets/Resources/Scripts/DungeonCarver.cs
+++ Assets/Resources/Scripts/DungeonCarver.cs
@@ -1,30 +1,93 @@
 ﻿namespace DungeonCarver
 {
-    using UnityEngine;
     using System;
-    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEditor;
 
     public class DungeonCarver : MonoBehaviour
     {
-        public enum Generators
-        {
-            BorderOnlyMapGenerator,
-            BSPTreeMapGenerator,
-            CaveMapGenerator,
-            CellularAutomataMapGenerator,
-            CityMapGenerator,
-            DFSMazeMapGenerator,
-            DrunkardsWalkMapGenerator,
-            TunnelingMazeMapGenerator,
-            TunnelingWithRoomsMapGenerator
-        };
-
-        public Generators selectedGenerator = Generators.BorderOnlyMapGenerator;
-
         public Transform dungeonParent = null;
         public GameObject tilePrefab = null;
         public Sprite wall = null;
         public Sprite empty = null;
+
+        //Generic Vars
+        [HideInInspector]
+        public Generators generator;
+        [HideInInspector]
+        public int mapWidth = 25;
+        [HideInInspector]
+        public int mapHeight = 25;
+
+        //BSP Tree Specific Var
+        [HideInInspector]
+        public int maxLeafSize = 24;
+        [HideInInspector]
+        public int roomMaxSize = 15;
+        [HideInInspector]
+        public int roomMinSize = 6;
+
+        //Cave System Specific Vars
+        [HideInInspector]
+        public int neighbours = 4;
+        [HideInInspector]
+        public int iterations = 50000;
+        [HideInInspector]
+        public int closeTileProb = 45;
+        [HideInInspector]
+        public int lowerLimit = 16;
+        [HideInInspector]
+        public int upperLimit = 500;
+        [HideInInspector]
+        public int emptyNeighbours = 3;
+        [HideInInspector]
+        public int emptyTileNeighbours = 4;
+        [HideInInspector]
+        public int corridorSpace = 2;
+        [HideInInspector]
+        public int corridorMaxTurns = 10;
+        [HideInInspector]
+        public int corridorMin = 2;
+        [HideInInspector]
+        public int corridorMax = 5;
+        [HideInInspector]
+        public int breakOut = 100000;
+
+        //Cellullar Automata Vars
+        [HideInInspector]
+        public int fillProbability = 50;
+        [HideInInspector]
+        public int totalIterations = 3;
+        [HideInInspector]
+        public int cutoffOfBigAreaFill = 3;
+
+        // City Vars
+        [HideInInspector]
+        public int maxCityLeafSize = 30;
+        [HideInInspector]
+        public int roomMaxCitySize = 16;
+        [HideInInspector]
+        public int roomMinCitySize = 8;
+        [HideInInspector]
+        public Vector2Int cityInsets = new Vector2Int(1, 1);
+
+        //Drunkards Walk
+        [HideInInspector]
+        public float percentGoal = 0.3f;
+        [HideInInspector]
+        public int walkIterations = 50000;
+        [HideInInspector]
+        public float weightedTowardCenter = 0.15f;
+        [HideInInspector]
+        public float weightedTowardPreviousDirection = 0.7f;
+
+        //Tunneling With Rooms
+        [HideInInspector]
+        public int maxTunnelingRooms = 30;
+        [HideInInspector]
+        public int roomMaxTunnelingSize = 15;
+        [HideInInspector]
+        public int roomMinTunnelingSize = 6;
 
         private IMap _map;
 
@@ -34,59 +97,59 @@
             System.Random random = new System.Random(DateTime.Now.Millisecond);
             IMapGenerator<Map> mapGenerator;
 
-            switch (selectedGenerator)
+            switch (generator)
             {
                 case Generators.BorderOnlyMapGenerator:
                     {
-                        mapGenerator = new BorderOnlyMapGenerator<Map>(50, 50);
+                        mapGenerator = new BorderOnlyMapGenerator<Map>(mapWidth, mapHeight);
                         _map = Map.Create(mapGenerator);
                         break;
                     }
                 case Generators.BSPTreeMapGenerator:
                     {
-                        mapGenerator = new BSPTreeMapGenerator<Map>(50, 50, 24, 15, 6, random);
+                        mapGenerator = new BSPTreeMapGenerator<Map>(mapWidth, mapHeight, maxLeafSize, roomMaxSize, roomMinSize, random);
                         _map = Map.Create(mapGenerator);
                         break;
                     }
                 case Generators.CaveMapGenerator:
                     {
-                        mapGenerator = new CaveMapGenerator<Map>(100, 100, 4, 50000, 45, 16, 500, 3, 4, 2, 10, 2, 5, 100000, random);
+                        mapGenerator = new CaveMapGenerator<Map>(mapWidth, mapHeight, neighbours, iterations, closeTileProb, lowerLimit, upperLimit, emptyNeighbours, emptyTileNeighbours, corridorSpace, corridorMaxTurns, corridorMin, corridorMax, breakOut, random);
                         _map = Map.Create(mapGenerator);
                         break;
                     }
                 case Generators.CellularAutomataMapGenerator:
                     {
-                        mapGenerator = new CellularAutomataMapGenerator<Map>(50, 50, 50, 3, 3, random);
+                        mapGenerator = new CellularAutomataMapGenerator<Map>(mapWidth, mapHeight, fillProbability, totalIterations, cutoffOfBigAreaFill, random);
                         _map = Map.Create(mapGenerator);
                         break;
                     }
                 case Generators.CityMapGenerator:
                     {
-                        mapGenerator = new CityMapGenerator<Map>(50, 50, 30, 16, 8, new Vector2Int(1, 1), random);
+                        mapGenerator = new CityMapGenerator<Map>(mapWidth, mapHeight, maxCityLeafSize, roomMaxCitySize, roomMinCitySize, cityInsets, random);
                         _map = Map.Create(mapGenerator);
                         break;
                     }
                 case Generators.DFSMazeMapGenerator:
                     {
-                        mapGenerator = new DFSMazeMapGenerator<Map>(50, 50, random);
+                        mapGenerator = new DFSMazeMapGenerator<Map>(mapWidth, mapHeight, random);
                         _map = Map.Create(mapGenerator);
                         break;
                     }
                 case Generators.DrunkardsWalkMapGenerator:
                     {
-                        mapGenerator = new DrunkardsWalkMapGenerator<Map>(80, 60, 0.3f, 50000, 0.15f, 0.7f, random);
+                        mapGenerator = new DrunkardsWalkMapGenerator<Map>(mapWidth, mapHeight, percentGoal, walkIterations, weightedTowardCenter, weightedTowardPreviousDirection, random);
                         _map = Map.Create(mapGenerator);
                         break;
                     }
                 case Generators.TunnelingMazeMapGenerator:
                     {
-                        mapGenerator = new TunnelingMazeMapGenerator<Map>(50, 50, random);
+                        mapGenerator = new TunnelingMazeMapGenerator<Map>(mapWidth, mapHeight, random);
                         _map = Map.Create(mapGenerator);
                         break;
                     }
                 case Generators.TunnelingWithRoomsMapGenerator:
                     {
-                        mapGenerator = new TunnelingWithRoomsMapGenerator<Map>(80, 60, 30, 15, 6, random);
+                        mapGenerator = new TunnelingWithRoomsMapGenerator<Map>(mapWidth, mapHeight, maxTunnelingRooms, roomMaxTunnelingSize, roomMinTunnelingSize, random);
                         _map = Map.Create(mapGenerator);
                         break;
                     }
---
+++ Assets/Resources/Scripts/InspectorVars.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEditor;
+namespace DungeonCarver
+{
+    public enum Generators
+    {
+        BorderOnlyMapGenerator,
+        BSPTreeMapGenerator,
+        CaveMapGenerator,
+        CellularAutomataMapGenerator,
+        CityMapGenerator,
+        DFSMazeMapGenerator,
+        DrunkardsWalkMapGenerator,
+        TunnelingMazeMapGenerator,
+        TunnelingWithRoomsMapGenerator
+    }
+
+    [CustomEditor(typeof(DungeonCarver))]
+    public class InspectorVars : Editor
+    {
+
+        private DungeonCarver _script;
+
+        void OnEnable()
+        {
+            _script = target as DungeonCarver;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            _script.generator = (Generators)EditorGUILayout.EnumPopup("Generators:", _script.generator);
+
+            _script.mapWidth = EditorGUILayout.IntField("Map Width:", _script.mapWidth);
+            _script.mapHeight = EditorGUILayout.IntField("Map Height:", _script.mapHeight);
+
+            switch (_script.generator)
+            {
+                case Generators.BSPTreeMapGenerator:
+                    {
+                        _script.maxLeafSize = EditorGUILayout.IntField("Max Leaf Size:", _script.maxLeafSize);
+                        _script.roomMaxSize = EditorGUILayout.IntField("Room Max Size:", _script.roomMaxSize);
+                        _script.roomMinSize = EditorGUILayout.IntField("Room Min Size:", _script.roomMinSize);
+                        break;
+                    }
+                case Generators.CaveMapGenerator:
+                    {
+
+
+                        _script.neighbours = EditorGUILayout.IntField("Neighbours:", _script.neighbours);
+                        _script.iterations = EditorGUILayout.IntField("Iterations:", _script.iterations);
+                        _script.closeTileProb = EditorGUILayout.IntField("CloseTileProb:", _script.closeTileProb);
+                        _script.lowerLimit = EditorGUILayout.IntField("Lower Limit:", _script.lowerLimit);
+                        _script.upperLimit = EditorGUILayout.IntField("Upper Limit:", _script.upperLimit);
+                        _script.emptyNeighbours = EditorGUILayout.IntField("Empty Neighbours:", _script.emptyNeighbours);
+                        _script.emptyTileNeighbours = EditorGUILayout.IntField("Empty Tile Neighbours:", _script.emptyTileNeighbours);
+                        _script.corridorSpace = EditorGUILayout.IntField("Corridor Space:", _script.corridorSpace);
+                        _script.corridorMaxTurns = EditorGUILayout.IntField("Corridor Max Turns:", _script.corridorMaxTurns);
+                        _script.corridorMin = EditorGUILayout.IntField("Corridor Min:", _script.corridorMin);
+                        _script.corridorMax = EditorGUILayout.IntField("Corridor Max:", _script.corridorMax);
+                        _script.breakOut = EditorGUILayout.IntField("BreakOut:", _script.breakOut);
+
+                        break;
+                    }
+                case Generators.CellularAutomataMapGenerator:
+                    {
+                        _script.fillProbability = EditorGUILayout.IntField("Fill Probability:", _script.fillProbability);
+                        _script.totalIterations = EditorGUILayout.IntField("Total Iterations:", _script.totalIterations);
+                        _script.cutoffOfBigAreaFill = EditorGUILayout.IntField("Cutoff Of Big Area Fill:", _script.cutoffOfBigAreaFill);
+                        break;
+                    }
+                case Generators.CityMapGenerator:
+                    {
+                        _script.maxCityLeafSize = EditorGUILayout.IntField("Max Leaf Size:", _script.maxCityLeafSize);
+                        _script.roomMaxCitySize = EditorGUILayout.IntField("Room Max Size:", _script.roomMaxCitySize);
+                        _script.roomMinCitySize = EditorGUILayout.IntField("Room Min Size:", _script.roomMinCitySize);
+                        _script.cityInsets = EditorGUILayout.Vector2IntField("Insets:", _script.cityInsets);
+                        break;
+                    }
+                case Generators.DrunkardsWalkMapGenerator:
+                    {
+                        _script.percentGoal = EditorGUILayout.FloatField("Percent Goal:", _script.percentGoal);
+                        _script.walkIterations = EditorGUILayout.IntField("Room Max Size:", _script.walkIterations);
+                        _script.weightedTowardCenter = EditorGUILayout.FloatField("Weighted Toward Center:", _script.weightedTowardCenter);
+                        _script.weightedTowardPreviousDirection = EditorGUILayout.FloatField("Weighted Toward Previous Direction:", _script.weightedTowardPreviousDirection);
+                        break;
+                    }
+                case Generators.TunnelingWithRoomsMapGenerator:
+                    {
+                        _script.maxTunnelingRooms = EditorGUILayout.IntField("Max Rooms:", _script.maxTunnelingRooms);
+                        _script.roomMaxTunnelingSize = EditorGUILayout.IntField("Room Max Size:", _script.roomMaxTunnelingSize);
+                        _script.roomMinTunnelingSize = EditorGUILayout.IntField("Room Min Size:", _script.roomMinTunnelingSize);
+                        break;
+                    }
+            }
+
+            // Save the changes back to the object
+            EditorUtility.SetDirty(target);
+        }
+
+    }
+}
